b81547fc0f3eb489f11c7470a8b283465c87e6db
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-05-11 16:10:49 -0300

    Criado migrate editora

---
+++ migrations/20200511190037-create_table_editora.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return queryInterface.createTable('editora', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nome: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+      }
+    });
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return queryInterface.dropTable('editora');
+  }
+};
