77e0cf68e5a8b9138f1111437ad815fbe8ce3f58
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-05-11 16:10:10 -0300

    Criado migrate usuario

---
+++ migrations/20200511174019-create_table_usuario.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return queryInterface.createTable('usuario',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nome: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+      }, 
+      senha: {
+        type: Sequelize.STRING(256),
+        allowNull: false
+      },
+    });
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+   return queryInterface.dropTable('usuario');    
+  }
+};
